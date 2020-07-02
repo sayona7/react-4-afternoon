51f2d4583aa80343192b53701089e6bfd901c91f
Veronica <weronika.bakanowicz@g.austincc.edu>
2020-07-02 18:30:11 -0500

    progress in homework with steps 5-9

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import {Switch, Route} from "react-router-dom";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import ClassList from "./components/ClassList/ClassList";
+import Student from "./components/Student/Student";
 
 export default (
     <Switch>
         <Route component={Home} exact path="/" />
         <Route component={About} exact path="/about" />
         <Route component={ClassList} exact path="/classlist/:class" />
+        <Route component={Student} exact path="/student/:id" />
     </Switch>
 );
